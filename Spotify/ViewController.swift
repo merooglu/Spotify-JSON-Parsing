362548708135853163ced9e2beeafc0c78582ede
Mehmet Eroglu <mehmet---eroglu@hotmail.com>
2017-03-22 12:07:11 +0300

    Spotify Json Parse

--- Spotify/ViewController.swift
+++ Spotify/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
 
+    var names = [String]()
+    
+    var searchURL = "https://api.spotify.com/v1/search?q=Shawn+Mendes&type=track"
+    typealias JSONStandard = [String : AnyObject]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        callAlamo(url: searchURL)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func callAlamo (url : String){
+        Alamofire.request(url).responseJSON(completionHandler: {
+            
+            response in
+            self.parseData(JSONData: response.data!)
+            
+        })
     }
-
-
+    
+    func parseData (JSONData: Data){
+        do {
+            let readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard {
+                if let items = tracks["items"]{
+                    for i in 0..<items.count{
+                        let item = items[i] as! JSONStandard
+                        let name = item["name"] as! String
+                        names.append(name)
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = names[indexPath.row]
+        return cell!
+    }
 }
 
